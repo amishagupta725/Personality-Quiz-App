f9d0e7eec65bc414a81f6c5bd3b85f52f152fa7d
guptabhoomika <bhoomika.189301123@muj.manipal.edu>
2020-10-22 10:34:04 +0530

    Color scheme

---
+++ lib/colors.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+Color firstColor = Color(0xFFF47D18);
+Color secondColor = Color(0xFFFF772E);
+
+ThemeData appTheme =
+ThemeData(primaryColor: Color(0xFFF3791A), fontFamily: 'Oxygen');
